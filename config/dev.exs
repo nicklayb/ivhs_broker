42b5625a624ec60a5af23819c4a44b2a3b9cce70
nicklayb <nicklay@me.com>
2026-08-28 17:13:00 -0400

    Dev env

--- config/dev.exs
+++ config/dev.exs
@@ -6,12 +6,21 @@ config :ivhs_broker, IvhsBroker.Repo,
 
 config :ivhs_broker, IvhsBrokerWeb.Endpoint,
   check_origin: false,
-  code_reloader: true,
+  # code_reloader: true,
   debug_errors: true,
   watchers: [
     esbuild: {Esbuild, :install_and_run, [:ivhs_broker, ~w(--sourcemap=inline --watch)]},
     tailwind: {Tailwind, :install_and_run, [:ivhs_broker, ~w(--watch)]}
-  ]
+  ],
+  live_reload: [
+    web_console_logger: true,
+    patterns: [
+      ~r"priv/static/(?!.*local_storage).*(js|css|png|jpeg|jpg|gif|svg|json)$",
+      ~r"priv/gettext/.*(po)$",
+      ~r"lib/ivhs_broker_web/.*/.*(ex|eex)$"
+    ]
+  ],
+  reloadable_compilers: [:gettext, :phoenix, :elixir]
 
 config :logger, :console, format: "[$level] $message\n"
 
